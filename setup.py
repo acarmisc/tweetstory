b30a7e3e665a7915de4cfc2c100448be90ff62bf
Andrea Carmisciano <andrea.carmisciano@infoporto.it>
2014-03-28 22:15:47 +0100

    none

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-<<<<<<< HEAD
 setup(name='TweetStory',
       version='0.1',
       description='Cache custom Tweet',
@@ -8,13 +7,4 @@ setup(name='TweetStory',
       author_email='andrea.carmisciano@gmail.com',
       url='https://github.com/acarmisc/tweetstory',
       install_requires=['MarkupSafe'],
-=======
-setup(name='YourAppName',
-      version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
->>>>>>> 1d660ebbf60bad71fc09bdf2b5571c27e0cf25da
      )
