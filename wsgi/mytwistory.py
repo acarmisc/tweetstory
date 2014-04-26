d9a28caef5751a70b27029b7d36641c241abb26d
Andrea Carmisciano <andrea.carmisciano@infoporto.it>
2014-04-26 17:48:06 +0200

    oauth test

--- wsgi/mytwistory.py
+++ wsgi/mytwistory.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 import datetime
 from bson.objectid import ObjectId
 import logging
 from tools import getConfig, dbConnect
 from twitter import twitterClient
+from flask_oauth import OAuth
 
 
 app = Flask(__name__)
@@ -17,10 +18,51 @@ tClient = twitterClient(config_dict=config['twitter'])
 logging.basicConfig(level=logging.DEBUG)
 
 
-@app.route("/create")
-def create():
-    return render_template('create.html')
+"""
+Twitter login part
+"""
 
+oauth = OAuth()
+twitter = oauth.remote_app('twitter',
+    base_url='https://api.twitter.com/1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    consumer_key=config['twitter']['consumer_key'],
+    consumer_secret=config['twitter']['consumer_secret']
+)
+
+
+@twitter.tokengetter
+def get_twitter_token(token=None):
+    return session.get('twitter_token')
+
+
+@app.route('/login')
+def login():
+    return twitter.authorize(callback=url_for('oauth_authorized',
+        next=request.args.get('next') or request.referrer or None))
+
+
+@app.route('/oauth-authorized')
+@twitter.authorized_handler
+def oauth_authorized(resp):
+    next_url = request.args.get('next') or url_for('index')
+    if resp is None:
+        flash(u'You denied the request to sign in.')
+        return redirect(next_url)
+
+    session['twitter_token'] = (
+        resp['oauth_token'],
+        resp['oauth_token_secret']
+    )
+    session['twitter_user'] = resp['screen_name']
+
+    flash('You were signed in as %s' % resp['screen_name'])
+    return redirect(next_url)
+
+
+""" Main applications logic starts here """
 
 @app.route('/save', methods=['POST'])
 def save():
@@ -37,7 +79,7 @@ def save():
 
     db.schedule.insert(tostore)
 
-    return welcome()
+    return list()
 
 
 @app.route("/list")
@@ -61,8 +103,9 @@ def show(id=None):
 
 @app.route("/")
 def welcome():
-    return render_template('welcome.html')
+    return list()
 
 
 if __name__ == "__main__":
+    app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
     app.run(debug=True)
