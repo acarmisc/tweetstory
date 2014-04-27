28493c599e8bf78bfd40524a042727573f2a4d8f
Andrea Carmisciano <andrea.carmisciano@infoporto.it>
2014-04-27 19:03:03 +0200

    boh

--- wsgi/agent.py
+++ wsgi/agent.py
@@ -6,21 +6,22 @@ store them to db.
 import datetime
 from twitter import twitterClient
 from tools import getConfig, dbConnect
-
+import time
+import logging
 
 config = getConfig()
 db = dbConnect(config['db_url'], config['db_name'])
 tClient = twitterClient(config_dict=config['twitter'])
 
-#while True:
-
 now = datetime.datetime.now()
-print now
 
 todo = db.schedule.find({'date_start': {'$lt': now},
                          'date_end': {'$gte': now}})
 
-#logging.debug("Fetching data at %s" % time.ctime())
+
+logging.basicConfig(level=logging.DEBUG)
+
+logging.debug("Fetching data at %s" % time.ctime())
 fetched = tClient.fetch(todo)
 
 #TODO: move to specific object
@@ -34,4 +35,28 @@ for f in fetched:
         except:
             pass
 
-#   time.sleep(30)
+"""
+while True:
+
+    now = datetime.datetime.now()
+    print now
+
+    todo = db.schedule.find({'date_start': {'$lt': now},
+                             'date_end': {'$gte': now}})
+
+    #logging.debug("Fetching data at %s" % time.ctime())
+    fetched = tClient.fetch(todo)
+
+    #TODO: move to specific object
+    history = db.history
+    for f in fetched:
+        # avoid duplicated twitt
+        found = history.find({'oid': f['oid']})
+        if found.count() == 0:
+            try:
+                history.insert(f)
+            except:
+                pass
+
+    time.sleep(30)
+"""
