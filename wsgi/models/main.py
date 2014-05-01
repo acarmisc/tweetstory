e6faac8ea7ee97a843721850ed6baf2a51671c32
Andrea Carmisciano <andrea.carmisciano@infoporto.it>
2014-05-01 21:56:08 +0200

    test

--- wsgi/models/main.py
+++
@@ -1,9 +0,0 @@
-from flask.ext.mongoengine import MongoEngine
-
-
-def getDb(config):
-    me = MongoEngine()
-    c = me.connect(config['db_name'], host=config['db_url'])
-    db = c.config['db_name']
-
-    return db
--- wsgi/mytwistory.py
+++ wsgi/mytwistory.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request, \
     session, redirect, url_for, flash
 from tools import getConfig, _logger
 from twitter import twitterClient
-from models.main import getDb
+from flask.ext.mongoengine import MongoEngine
 
 
 config = getConfig()
@@ -16,7 +16,7 @@ app.config['MONGODB_SETTINGS'] = {'DB': config['db_name'],
 tClient = twitterClient(config_dict=config['twitter'])
 twitter = tClient.authenticate()
 
-db = getDb(config)
+db = MongoEngine()
 
 """ basic functions """
 
