0510047794d9944a829973f8ec280759f610533c
Andrea Carmisciano <andrea.carmisciano@gmail.com>
2014-06-19 21:56:43 +0200

    gui effects

--- wsgi/lib/twitter.py
+++ wsgi/lib/twitter.py
@@ -24,6 +24,12 @@ class twitterClient(object):
 
         return api
 
+    def get_auth(self):
+        auth = tweepy.OAuthHandler(self.t_api_key, self.t_api_secret)
+        auth.set_access_token(self.t_acc_token, self.t_acc_secret)
+
+        return auth
+
     def authenticate(self):
         oauth = OAuth()
         twitter = oauth.remote_app('twitter',
@@ -40,6 +46,7 @@ class twitterClient(object):
     def fetch(self, todo):
         fetched = []
         api = self.connect()
+
         done = []
 
         for el in todo:
@@ -82,3 +89,58 @@ class twitterClient(object):
         user = api.get_user(screen_name, include_entities=1)
 
         return user
+
+    def fetch_stream(self, todo):
+        fetched = []
+        auth = self.get_auth()
+        stream = tweepy.Stream(auth, StreamWatcherListener(), timeout=None)
+
+        track_list = []
+
+        for el in todo:
+            track_list.append(el.hashtag)
+
+        follow_list = None
+
+        found = 0
+        stream.filter(follow_list, track_list)
+
+
+class StreamWatcherListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        try:
+            print status
+            hashtags = []
+            media = []
+
+            for h in r.entities['hashtags']:
+                hashtags.append(h['text'])
+
+            if 'media' in r.entities:
+                for m in r.entities['media']:
+                    media.append(m['media_url'])
+
+            data = {'oid': str(r.id),
+                    'text': r.text,
+                    'author': r.author.screen_name,
+                    'avatar': r.user.profile_image_url_https,
+                    'screen_name': r.user.screen_name,
+                    'hashtags': hashtags,
+                    'created_at': r.created_at,
+                    'retweet_count': r.retweet_count,
+                    'media': media}
+
+            return data
+
+        except:
+            # Catch any unicode errors while printing to console
+            # and just ignore them to avoid breaking application.
+            pass
+
+    def on_error(self, status_code):
+        print 'An error has occured! Status code = %s' % status_code
+        return True  # keep stream alive
+
+    def on_timeout(self):
+        print 'Snoozing Zzzzzz'
