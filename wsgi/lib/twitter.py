20551fa120339706b2bb678c366801ff58bd0bc9
Andrea Carmisciano <andrea.carmisciano@gmail.com>
2014-06-20 00:15:46 +0200

    sss

--- wsgi/lib/twitter.py
+++ wsgi/lib/twitter.py
@@ -32,6 +32,8 @@ class twitterClient(object):
 
     def authenticate(self):
         oauth = OAuth()
+        print config['twitter']['t_api_key']
+        print config['twitter']['t_api_secret']
         twitter = oauth.remote_app('twitter',
             base_url='https://api.twitter.com/1/',
             request_token_url='https://api.twitter.com/oauth/request_token',
@@ -89,6 +91,11 @@ class twitterClient(object):
         user = api.get_user(screen_name, include_entities=1)
 
         return user
+
+    def write_tweet(self, msg):
+        api = self.connect()
+        return True if api.update_status(msg) else False
+
 
     def fetch_stream(self, todo):
         fetched = []
